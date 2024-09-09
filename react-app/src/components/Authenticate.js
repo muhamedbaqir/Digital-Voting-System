8862a51fc04330d2eff69e862af5ae0dbbfd0d8f
muhamedbaqir <muhamedbaqir32@gmail.com>
2024-09-09 18:28:52 +0200

    made authenticate prettier

--- react-app/src/components/Authenticate.js
+++ react-app/src/components/Authenticate.js
@@ -17,42 +17,60 @@ export default function Authenticate() {
 
   return (
     <>
-      <h1>Authenticate</h1>
-      <form className="form-control justify-content-center align-items-center">
-        <div className="mb-3">
-          <label htmlFor="firstName">First Name:</label>
-          <input type="text" id="firstName" />
-        </div>
+      <div className="container center-text mt-5">
+        <h1>Authenticate</h1>
+        <form className="form-control justify-content-center align-items-center">
+          <div className="mb-3">
+            <label className="form-label" htmlFor="firstName">
+              First Name
+            </label>
+            <input className="form-control" type="text" id="firstName" />
+          </div>
 
-        <div className="mb-3">
-          <label htmlFor="lastName">Last Name:</label>
-          <input type="text" id="lastName" />
-        </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="lastName">
+              Last Name
+            </label>
+            <input className="form-control" type="text" id="lastName" />
+          </div>
 
-        <div className="mb-3">
-          <label htmlFor="street"> Street:</label>
-          <input type="text" id="street" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="streetNumber">Street Number:</label>
-          <input type="number" id="streetNumber" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="postalCode">Postal Code:</label>
-          <input type="number" id="postalCode" />
-        </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="street">
+              Street
+            </label>
+            <input className="form-control" type="text" id="street" />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="streetNumber">
+              Street Number
+            </label>
+            <input className="form-control" type="number" id="streetNumber" />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="postalCode">
+              Postal Code
+            </label>
+            <input className="form-control" type="number" id="postalCode" />
+          </div>
 
-        <div className="mb-3">
-          <label htmlFor="birthday">Birthday:</label>
-          <input type="Date" id="birthday" />
-        </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="birthday">
+              Birthday
+            </label>
+            <input className="form-control" type="Date" id="birthday" />
+          </div>
 
-        <Link to="/vote">
-          <button type="submit" onSubmit={handleSubmit()}>
-            Foo
-          </button>
-        </Link>
-      </form>
+          <Link to="/vote">
+            <button
+              className="btn btn-primary"
+              type="submit"
+              onSubmit={handleSubmit()}
+            >
+              Submit
+            </button>
+          </Link>
+        </form>
+      </div>
     </>
   );
 }
