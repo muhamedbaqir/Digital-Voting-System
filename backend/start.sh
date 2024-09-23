7916cb9259cacb8a9e4c5670925f264e980a272c
muhamedbaqir <muhamedbaqir32@gmail.com>
2024-09-23 22:47:36 +0200

    changed port of uvicorn command (fastapi)

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-exec uvicorn app.main:app --host 0.0.0.0 --port ${SERVER_PORT}
+#exec uvicorn app.main:app --host 0.0.0.0 --port ${SERVER_PORT}
+exec uvicorn app.main:app --host 127.0.0.1 --port 8000
