308745cc83066bc9dad02ad715d660f40703d11e
muhamedbaqir <muhamedbaqir32@gmail.com>
2024-09-23 23:33:39 +0200

    managed to connect nginx to backends, need to move into vars and test load balancing

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#exec uvicorn app.main:app --host 0.0.0.0 --port ${SERVER_PORT}
-exec uvicorn app.main:app --host 127.0.0.1 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port ${SERVER_PORT}
