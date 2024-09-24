d108b4628f2e47ed7f0a27431035b8d838ab820e
muhamedbaqir <muhamedbaqir32@gmail.com>
2024-09-24 22:51:02 +0200

    refactored admin dashboard

--- react-app/src/pages/AdminDashboard.js
+++ react-app/src/pages/AdminDashboard.js
@@ -1,6 +1,31 @@
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
+const electionRunningPage = (navigate) => {
+  return (
+    <div>
+      <h1 className="text-primary">Election is running</h1>
+      <br />
+      <button className="btn btn-primary btn-lg mt-2">Stop Election</button>
+    </div>
+  );
+};
+
+const noElectionRunningPage = (navigate) => {
+  return (
+    <div>
+      <h1 className="text-primary">No Election is running</h1>
+      <br />
+      <button
+        className="btn btn-primary btn-lg mt-2"
+        onClick={() => navigate("/admin/AddConstituencies")}
+      >
+        Start Election
+      </button>
+    </div>
+  );
+};
+
 export default function AdminDashboard() {
   const [adminLoggedIn, setAdminLoggedIn] = useState(true);
   const [electionIsRunning, setElectionIsRunning] = useState(false);
@@ -12,26 +37,9 @@ export default function AdminDashboard() {
     <>
       <div className="container text-center mt-5">
         <h1 className="text-primary">Admin dashboard</h1>
-        {electionIsRunning ? (
-          <div>
-            <text>Election is running</text>
-            <br />
-            <button className="btn btn-primary btn-lg mt-2">
-              Stop Election
-            </button>
-          </div>
-        ) : (
-          <div>
-            <text>No election is running</text>
-            <br />
-            <button
-              className="btn btn-primary btn-lg mt-2"
-              onClick={() => navigate("/admin/AddConstituencies")}
-            >
-              Start Election
-            </button>
-          </div>
-        )}
+        {electionIsRunning
+          ? electionRunningPage(navigate)
+          : noElectionRunningPage(navigate)}
       </div>
     </>
   );
