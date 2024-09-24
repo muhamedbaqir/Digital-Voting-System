f632d003428fbf3617440ff2893aa3af289347e2
G-M3n <tizian.grossmann@priokom.de>
2024-09-24 19:53:08 +0200

    adding parties, cons_parties

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,8 @@ from app.database import session
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from app.routers.candidates import router as candidates_router
 from app.routers.constituencies import router as constituencies_router
-from app.routers.elections import router as elections_router
+from app.routers.constituency_parties import router as constituency_parties_router
+from app.routers.parties import router as parties_router
 from app.routers.voters import router as voters_router
 from app.routers.votes import router as votes_router
 from app.routers.admin import router as admin_router
@@ -11,7 +12,8 @@ from app.routers.admin import router as admin_router
 app = FastAPI()
 app.include_router(candidates_router, prefix="/candidates")
 app.include_router(constituencies_router, prefix="/constituencies")
-app.include_router(elections_router, prefix="/elections")
+app.include_router(constituency_parties_router, prefix="/constituency_parties")
+app.include_router(parties_router, prefix="/parties")
 app.include_router(voters_router, prefix="/voters")
 app.include_router(votes_router, prefix="/votes")
 app.include_router(admin_router, prefix="/admin")
@@ -20,13 +22,3 @@ app.include_router(admin_router, prefix="/admin")
 @app.get("/")
 def read_home():
     return {"message": "Hello!"}
-
-@app.get("/data")
-def get_data():
-    rows = session.execute("SELECT * FROM voters")
-    return {"data": [row for row in rows]}
-'''
-@app.post("/login")
-def login(user: OAuth2PasswordRequestForm = Depends()):
-    return login(sessin,user)
-    '''
--- backend/app/routers/candidates.py
+++ backend/app/routers/candidates.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from uuid import UUID
+from uuid import UUID, uuid4
 from typing import List
 from app.database import session
 from app.basemodels import Candidate
@@ -7,13 +7,11 @@ from app.basemodels import Candidate
 router = APIRouter()
 
 @router.post("/", response_model=Candidate)
-async def create_candidate(candidate: Candidate):
-    query = """
-    INSERT INTO candidates (candidate_id, name, party, position, campaign_slogan)
-    VALUES (%s, %s, %s, %s, %s)
-    """
-    session.execute(query, (candidate.candidate_id, candidate.name, candidate.party, candidate.position, candidate.campaign_slogan))
-    return candidate
+def create_candidate(candidate: Candidate):
+    candidate_id = uuid4()
+    query = "INSERT INTO candidates (candidate_id, name, constituency_party_id, constituency_id) VALUES (%s, %s, %s, %s)"
+    session.execute(query, (candidate_id, candidate.name, candidate.constituency_party_id, candidate.constituency_id))
+    return {"candidate_id": candidate_id, "name": candidate.name, "constituency_party_id": candidate.constituency_party_id, "constituency_id": candidate.constituency_id}
 
 @router.get("/{candidate_id}", response_model=Candidate)
 async def read_candidate(candidate_id: UUID):
@@ -27,16 +25,15 @@ async def read_candidate(candidate_id: UUID):
 async def update_candidate(candidate_id: UUID, candidate: Candidate):
     query = """
     UPDATE candidates
-    SET name=%s, party=%s, position=%s, campaign_slogan=%s
+    SET name=%s, party_id=%s, constituency_id=%s
     WHERE candidate_id=%s
     """
-    session.execute(query, (candidate.name, candidate.party, candidate.position, candidate.campaign_slogan, candidate_id))
+    session.execute(query, (candidate.name, candidate.constituency_party_id, candidate.constituency_id, candidate_id))
     return Candidate(
         candidate_id=candidate_id,
         name=candidate.name,
-        party=candidate.party,
-        position=candidate.position,
-        campaign_slogan=candidate.campaign_slogan
+        party_id=candidate.constituency_party_id,
+        constituency_id=candidate.constituency_id
     )
 
 @router.delete("/{candidate_id}", response_model=Candidate)
@@ -50,9 +47,9 @@ async def delete_candidate(candidate_id: UUID):
     session.execute(query, (candidate_id,))
     return Candidate(**row._asdict())
 
-@router.get("/", response_model=List[Candidate])
-async def list_candidates():
-    query = "SELECT * FROM candidates"
-    rows = session.execute(query)
+@router.get("/fromconstituency/{constituency_id}", response_model=List[Candidate])
+async def list_candidates(constituency_id: UUID):
+    query = "SELECT * FROM candidates WHERE constituency_id=%s"
+    rows = session.execute(query,(constituency_id, ))
     candidates = [Candidate(**row._asdict()) for row in rows]
     return candidates
