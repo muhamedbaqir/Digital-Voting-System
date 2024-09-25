9bb69a9520ce7ea68724ac15e38f937e6bb5beff
muhamedbaqir <muhamedbaqir32@gmail.com>
2024-09-25 14:03:58 +0200

    ui add constituency

--- react-app/src/pages/StartElection/AddConstituencyParties.js
+++ react-app/src/pages/StartElection/AddConstituencyParties.js
@@ -1,30 +1,88 @@
 import { useNavigate } from "react-router-dom";
 import PrevNextButtonBar from "../../components/PrevNextButtonBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const displayConstituencyParties = (constituencyParties, handleDelete) => {
+  console.log(constituencyParties);
+  return (
+    <div>
+      {constituencyParties.map((c) => (
+        <div>
+          <h2 className="text-secondary">{c.name}</h2>
+          <ul className="list-group">
+            {c.parties.map((p) => (
+              <li class="list-group-item mt-3">
+                <div className="row">
+                  <div className="col">{p}</div>
+                  <column className="col">
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(c.name, p)}
+                    >
+                      delete
+                    </button>
+                  </column>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default function AddConstituencyParties() {
   const navigate = useNavigate();
   const [constituencyParties, setConstituencyParties] = useState([
     { name: "berlin", parties: ["spd"] },
-    { name: "münchen", parties: ["csu, afd"] },
+    { name: "münchen", parties: ["csu", "afd"] },
   ]);
+
+  useEffect(() => {}, [constituencyParties]);
+
   const [searchResult, setSearchResult] = useState("");
-  const [selectedConstituency, setSelectedConstituency] = useState("");
+  const [selectedConstituency, setSelectedConstituency] = useState(
+    constituencyParties[0].name
+  );
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(searchResult);
-    console.log(selectedConstituency);
+    let index = constituencyParties.findIndex(
+      (it) => it.name == selectedConstituency
+    );
+    let tempParties = constituencyParties[index].parties;
+    if (tempParties.includes(searchResult) || searchResult == "") {
+      alert("Party is null or already exists");
+      return;
+    }
+    tempParties = [...tempParties, searchResult];
+    let tempConstituencies = [...constituencyParties];
+    tempConstituencies[index].parties = tempParties;
+    setConstituencyParties(tempConstituencies);
+    setSearchResult("");
+  };
+
+  const handleDelete = (constituencyName, party) => {
+    let constituencyIndex = constituencyParties.findIndex(
+      (it) => it.name == constituencyName
+    );
+    let tempParties = [...constituencyParties[constituencyIndex].parties];
+    tempParties = tempParties.filter((it) => it != party);
+
+    let tempConstituencies = [...constituencyParties];
+    tempConstituencies[constituencyIndex].parties = [...tempParties];
+    setConstituencyParties([...tempConstituencies]);
   };
 
   return (
     <>
       <h1 className="text-primary">Constituency Parties</h1>
-      <form
-        class="d-inline-flex"
-        onSubmit={handleSubmit}
-        onChange={(e) => setSelectedConstituency(e.target.value)}
-      >
-        <select class="form-select" aria-label="Default select example">
+      <form class="d-inline-flex" onSubmit={handleSubmit}>
+        <select
+          class="form-select"
+          aria-label="Default select example"
+          onChange={(e) => setSelectedConstituency(e.target.value)}
+        >
           {constituencyParties.map((it) => (
             <option value={it.name}>{it.name}</option>
           ))}
@@ -41,6 +99,7 @@ export default function AddConstituencyParties() {
           Add
         </button>
       </form>
+      {displayConstituencyParties(constituencyParties, handleDelete)}
 
       <div className="fixed-bottom">
         {PrevNextButtonBar(
