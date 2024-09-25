ea73622b7dc661f15e8204d59302f49304ac2414
muhamedbaqir <muhamedbaqir32@gmail.com>
2024-09-25 02:05:51 +0200

    ui federal parties

--- react-app/src/pages/StartElection/AddConstituencyParties.js
+++ react-app/src/pages/StartElection/AddConstituencyParties.js
@@ -1,12 +1,47 @@
 import { useNavigate } from "react-router-dom";
 import PrevNextButtonBar from "../../components/PrevNextButtonBar";
+import { useState } from "react";
 
 export default function AddConstituencyParties() {
   const navigate = useNavigate();
+  const [constituencyParties, setConstituencyParties] = useState([
+    { name: "berlin", parties: ["spd"] },
+    { name: "münchen", parties: ["csu, afd"] },
+  ]);
+  const [searchResult, setSearchResult] = useState("");
+  const [selectedConstituency, setSelectedConstituency] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchResult);
+    console.log(selectedConstituency);
+  };
 
   return (
     <>
-      AddConstituencyParties
+      <h1 className="text-primary">Constituency Parties</h1>
+      <form
+        class="d-inline-flex"
+        onSubmit={handleSubmit}
+        onChange={(e) => setSelectedConstituency(e.target.value)}
+      >
+        <select class="form-select" aria-label="Default select example">
+          {constituencyParties.map((it) => (
+            <option value={it.name}>{it.name}</option>
+          ))}
+        </select>
+        <input
+          name="newEntry"
+          class="form-control me-2"
+          placeholder="Party"
+          id="searchResult"
+          value={searchResult}
+          onChange={(e) => setSearchResult(e.target.value)}
+        />
+        <button class="btn btn-outline-success" type="submit">
+          Add
+        </button>
+      </form>
+
       <div className="fixed-bottom">
         {PrevNextButtonBar(
           () => {
--- react-app/src/pages/StartElection/AddFederalParties.js
+++ react-app/src/pages/StartElection/AddFederalParties.js
@@ -1,11 +1,60 @@
 import { useNavigate } from "react-router-dom";
 import PrevNextButtonBar from "../../components/PrevNextButtonBar";
+import { useEffect, useState } from "react";
 
 export default function AddFederalParties() {
   const navigate = useNavigate();
+  const [parties, setParties] = useState([]);
+  const [searchResult, setSearchResult] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (parties.includes(searchResult) || searchResult == "") {
+      alert("Input cannot be null or already exists");
+      return;
+    }
+    setParties([...parties, searchResult]);
+    setSearchResult("");
+  };
+  const handleDelete = (e) => {
+    setParties(parties.filter((it) => it != e));
+  };
+
+  useEffect(() => {}, [parties]);
+
   return (
     <>
-      AddFederalParties{" "}
+      <h1 className="text-primary">Federal Parties</h1>
+      <form class="d-inline-flex" onSubmit={handleSubmit}>
+        <input
+          name="newEntry"
+          class="form-control me-2"
+          placeholder="Party"
+          id="searchResult"
+          value={searchResult}
+          onChange={(e) => setSearchResult(e.target.value)}
+        />
+        <button class="btn btn-outline-success" type="submit">
+          Add
+        </button>
+      </form>
+      <ul class="list-group">
+        {parties.map((it) => (
+          <li class="list-group-item mt-3">
+            <div className="row">
+              <div className="col">{it}</div>
+              <column className="col">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(it)}
+                >
+                  delete
+                </button>
+              </column>
+            </div>
+          </li>
+        ))}
+      </ul>
       <div className="fixed-bottom">
         {" "}
         {PrevNextButtonBar(
